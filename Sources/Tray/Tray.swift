4c5b8189f0cd5ce203d837120f5f7a9b47797b4c
boybeak <boybeak@gmail.com>
2024-06-08 17:11:34 +0800

    new version

--- Sources/Tray/Tray.swift
+++ Sources/Tray/Tray.swift
@@ -6,46 +6,58 @@ import SwiftUI
 
 public class Tray: NSObject {
     
+    public static func install(icon: NSImage, onConfig: (_ tray: Tray) -> Void)-> Tray {
+        let tray = Tray()
+        tray.setIcon(icon: icon)
+        onConfig(tray)
+        return tray
+    }
+    
+    @available(macOS 11.0, *)
+    public static func install(systemSymbolName: String, onConfig: (_ tray: Tray) -> Void)-> Tray {
+        let tray = Tray()
+        tray.setIcon(systemSymbolName: systemSymbolName)
+        onConfig(tray)
+        return tray
+    }
+
+    public static func install(named: String, onConfig: (_ tray: Tray) -> Void)-> Tray {
+        let tray = Tray()
+        tray.setIcon(named: named)
+        onConfig(tray)
+        return tray
+    }
+    
     private(set) var statusItem: NSStatusItem?
-    private(set) var popover = NSPopover()
+    private(set) var popover: NSPopover? = nil
     
     private(set) var width: Int = 0
     private(set) var height: Int = 0
-    private(set) var level: NSWindow.Level = .floating
     
+    private(set) var viewController: NSViewController? = nil
+    private var level: NSWindow.Level = .floating
     private var rightMenu: NSMenu? = nil
     
-    public override init(){}
+    private var onLeftClick: (() -> Bool)? = nil
+    private var onRightClick: (() -> Bool)? = nil
     
-    public func setSize(width: Int, height: Int)-> Tray {
-        self.width = width
-        self.height = height
-        return self
+    private override init() {}
+    
+    @available(macOS 11.0, *)
+    public func setIcon(systemSymbolName: String) {
+        let icon = NSImage(systemSymbolName: systemSymbolName, accessibilityDescription: nil)!
+        self.setIcon(icon: icon)
     }
     
-    public func setLevel(level: NSWindow.Level)-> Tray {
-        self.level = level
-        return self
+    public func setIcon(named: String) {
+        let icon = NSImage(named: named)!
+        self.setIcon(icon: icon)
     }
     
-    public func install(icon: NSImage, view: NSView, menu: NSMenu? = nil) throws {
-        
-        if self.width <= 0 || self.height <= 0 {
-            throw InvalidSizeError(width: self.width, height: self.height)
+    public func setIcon(icon: NSImage) {
+        if statusItem == nil {
+            statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         }
-        
-        self.rightMenu = menu
-        
-        popover.animates = true
-        popover.behavior = .transient
-        
-        popover.contentViewController = NSViewController()
-        popover.contentViewController?.view = view
-        popover.contentViewController?.view.frame = NSRect(x: 0, y: 0, width: self.width, height: self.height)
-        popover.contentViewController?.view.window?.level = self.level
-        
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
         if let menuBtn = statusItem?.button {
             icon.isTemplate = true
             menuBtn.image = icon
@@ -55,38 +67,77 @@ public class Tray: NSObject {
         }
     }
     
-    @objc func menuBtnAction(sender: AnyObject) {
+    public func setView(viewController: NSViewController, behavior: NSPopover.Behavior = .transient, level: NSWindow.Level = .floating, size: CGSize? = nil) {
+        self.viewController = viewController
+        if popover == nil {
+            popover = NSPopover()
+            popover?.animates = true
+            popover?.behavior = behavior
+        }
+        popover?.contentViewController = viewController
+        self.level = level
+        if size != nil {
+            popover?.contentViewController?.view.frame = NSRect(x: 0, y: 0, width: size!.width, height: size!.height)
+        }
+        
+    }
+        
+    public func setView(view: NSView, behavior: NSPopover.Behavior = .transient, level: NSWindow.Level = .floating, size: CGSize? = nil) {
+        let viewController = NSViewController()
+        viewController.view = view
+        self.setView(viewController: viewController, behavior: behavior, level: level, size: size)
+    }
+    
+    @available(macOS 10.15, *)
+    public func setView<Content: View>(content: Content, behavior: NSPopover.Behavior = .transient, level: NSWindow.Level = .floating, size: CGSize? = nil) {
+        self.setView(view: NSHostingView(rootView: content), behavior: behavior, level: level, size: size)
+    }
+    
+    public func setMenu(menu: NSMenu) {
+        self.rightMenu = menu
+    }
+    
+    public func setOnLeftClick(onClick: @escaping () -> Bool) {
+        self.onLeftClick = onClick
+    }
+    public func setOnRightClick(onClick: @escaping () -> Bool) {
+        self.onRightClick = onClick
+    }
+    
+    @objc private func menuBtnAction(sender: AnyObject) {
         guard let event = NSApp.currentEvent else { return }
         switch event.type {
         case .rightMouseUp:
+            if let rightClick = self.onRightClick {
+                if (rightClick()) {
+                    return
+                }
+            }
             if rightMenu != nil {
                 statusItem?.popUpMenu(rightMenu!)
             } else {
                 togglePopover(sender: sender)
             }
         default:
+            if let leftClick = self.onLeftClick {
+                if (leftClick()) {
+                    return
+                }
+            }
             togglePopover(sender: sender)
         }
     }
     
     private func togglePopover(sender: AnyObject) {
-        if popover.isShown {
-            popover.performClose(sender)
+        if popover?.isShown == true {
+            popover?.performClose(sender)
         } else {
             if let menuBtn = statusItem?.button {
-                popover.show(relativeTo: menuBtn.bounds, of: menuBtn, preferredEdge: .minY)
-                popover.contentViewController?.view.window?.makeKey()
+                popover?.show(relativeTo: menuBtn.bounds, of: menuBtn, preferredEdge: .minY)
+                popover?.contentViewController?.view.window?.level = self.level
+                popover?.contentViewController?.view.window?.makeKey()
             }
         }
     }
     
-}
-
-struct InvalidSizeError: Error {
-    let width: Int
-    let height: Int
-    init(width: Int, height: Int) {
-        self.width = width
-        self.height = height
-    }
 }
